05e5e06ce8f100a3852e0e0faa757e8ffb193a4f
Andrew <a.thibs98@gmail.com>
2025-04-01 23:26:18 -0600

    fixed spacing issue on login page

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -12,100 +12,118 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: LayoutBuilder(
-        builder: (context, constraints) => SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(minHeight: constraints.maxHeight),
-            child: IntrinsicHeight(
-              child: Container(
-                width: MediaQuery.sizeOf(context).width,
-                color: AppColors.white,
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Image.asset('assets/images/ripple-logo-sm.png'),
-                        Text(
-                          'ripple',
-                          style: GoogleFonts.montserrat(
-                              color: AppColors.black, fontSize: 40),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        LoginForm(),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Divider(
-                                color: AppColors.lightGray,
-                                thickness: 1.0,
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Text(
-                                'Or',
-                                style: GoogleFonts.raleway(
-                                    color: AppColors.black, fontSize: 20),
-                              ),
-                            ),
-                            Expanded(
-                              child: Divider(
-                                color: AppColors.lightGray,
-                                thickness: 1.0,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        CustomIconButton(
-                            text: 'Sign up',
-                            colors: [AppColors.lightGray, AppColors.lightGray],
-                            icon: null,
-                            function: () {
-                              Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => OnboardView()));
-                            }),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              'Terms and Conditions',
-                              style: GoogleFonts.lato(
-                                  textStyle: TextStyle(
-                                      color: AppColors.black, fontSize: 16)),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Privacy Policy',
-                              style: GoogleFonts.lato(
-                                  textStyle: TextStyle(
-                                      color: AppColors.black, fontSize: 16)),
-                            )
-                          ],
-                        )
-                      ],
-                    )
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 120.0, 16.0, 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Image.asset('assets/images/ripple-logo-sm.png'),
+                  Text(
+                    'ripple',
+                    style: GoogleFonts.montserrat(
+                        color: AppColors.black, fontSize: 40),
+                  ),
+                  // const SizedBox(
+                  //   height: 20,
+                  // ),
+                  // Text(
+                  //   textAlign: TextAlign.center,
+                  //   'Making waves for good,\npennies at a time',
+                  //   style: GoogleFonts.lato(
+                  //     color: AppColors.black,
+                  //     fontSize: 16,
+                  //     fontStyle: FontStyle.italic,
+                  //   ),
+                  // ),
+                ],
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                textAlign: TextAlign.center,
+                'Making waves for good,\npennies at a time',
+                style: GoogleFonts.lato(
+                  color: AppColors.black,
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
                 ),
               ),
-            ),
+              const SizedBox(
+                height: 50,
+              ),
+              Column(
+                children: [
+                  LoginForm(),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: AppColors.lightGray,
+                          thickness: 1.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Text(
+                          'Or',
+                          style: GoogleFonts.raleway(
+                              color: AppColors.black, fontSize: 20),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: AppColors.lightGray,
+                          thickness: 1.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  CustomIconButton(
+                      text: 'Sign up',
+                      colors: [AppColors.lightGray, AppColors.lightGray],
+                      icon: null,
+                      function: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => OnboardView()));
+                      }),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'Terms and Conditions',
+                        style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                color: AppColors.black, fontSize: 16)),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Privacy Policy',
+                        style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                color: AppColors.black, fontSize: 16)),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
